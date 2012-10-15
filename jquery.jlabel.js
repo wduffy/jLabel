076ef9f38143cdda24459b2e9c3ff56dc4332c83
William Duffy <info@wduffy.co.uk>
2012-10-15 17:56:04 +0200

    Update jquery.jlabel.js

--- jquery.jlabel.js
+++ jquery.jlabel.js
@@ -92,7 +92,7 @@
 								.text($input.attr('title'));
 			};
 			
-			// If this is a new label insert it into the DOM, if not then move it directly before it's related input
+			// If this is a new label insert it into the DOM, if not then move it directly before its related input
 			$input.before($label);
 
 			// Style the label to mimic it's textbox formatting
